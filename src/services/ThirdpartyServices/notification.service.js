cd30b42780266785f842f3038f12b3b09136740e
TAdejuwon <tadejuwon@chapelhilldenham.com>
2023-01-14 12:00:07 +0100

    it works fix

--- src/services/ThirdpartyServices/notification.service.js
+++ src/services/ThirdpartyServices/notification.service.js
@@ -1,12 +1,13 @@
 const { axiosPOST } = require('../../utils/request');
+const axios = require('axios');
 
 class NotificationService {
   static sendSMS = async (payload) => {
+    console.log(payload);
     const response = await axiosPOST(
       `${process.env.NOTIFICATION_BASE_URL}/api/v1/notification/sms`,
       payload,
       {
-        'Content-Type': ['application/json', 'application/json'],
         Authorization: `Bearer ${process.env.SERVICE_TOKEN}`,
       }
     );
